52536e842ae31205615417f733e5c7eccfe1a8b9
Bharadwaj <ndb@mail.com>
2024-06-15 17:08:47 +0530

    swapingvaraiblescode

---
+++ Swap_Two_Variables_NDB_copy.py
@@ -0,0 +1,8 @@
+# Python program to Swap Two variables
+number1=input('Enter 1st number:')
+number2=input('Enter 2nd number:')
+temp=number1
+number1=number2
+number2=temp
+print("Number 1:",number1)
+print("Number 2:", number2)
